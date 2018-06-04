0fdd8e3aa23a920fdecd1a3a9c5d1c2a80d4cb1f
marcwhittaker <home@marcwhittaker.co.uk>
2018-06-04 21:53:12 +0100

    Update

--- HelloAccrington/Program.cs
+++ HelloAccrington/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace HelloAccrington
 {
@@ -11,12 +12,20 @@ namespace HelloAccrington
         public string pickup = "Accrington";
         public string destination = "Haslingden";
         public int distance = 10;
+                
     }
     class Program
     {
         public static void Main(string[] args)
         {
             Job newjob = new Job();
+
+            //display system time direct to console
+            Console.WriteLine(DateTime.Now);
+
+            //var time = DateTime.Parse("yyyyMMdd");
+            //Console.WriteLine(time);
+
             Console.WriteLine("Please Enter Pickup Location");
             newjob.pickup = Console.ReadLine();
             Console.WriteLine("Please Enter Destination");
@@ -25,12 +34,19 @@ namespace HelloAccrington
             string inputdist = Console.ReadLine();
             int.TryParse(inputdist, out newjob.distance);
             Console.WriteLine(newjob.distance);
-            int time = newjob.distance * 2;
+            int duration = newjob.distance * 2;
             Console.ReadKey();
             Console.Clear();
             Console.WriteLine("Taxi is required for " + newjob.pickup + " going to " + newjob.destination);
-            Console.WriteLine("This should take " + time + " Minutes");
+            Console.WriteLine("This should take " + duration + " Minutes");
             Console.ReadKey();
+
+            //Simple array
+            string[] today = {"Monday","Tuesday","Wednesday"};
+            Console.WriteLine(today[0]);
+
+            Console.ReadKey();
+
         }
     }
 }
